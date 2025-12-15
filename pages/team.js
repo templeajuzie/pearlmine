257dfe60f165c5b0765df5f48b416bbba81d2a83
Temple <templeajuzie@gmail.com>
2025-12-15 12:31:53 +0100

    feat: new update

--- pages/team.js
+++ pages/team.js
@@ -1,12 +1,12 @@
 import Footer from "@/components/footer/Footer";
-import Team from "@/components/Team/Team";
+
 import Navbar from "../components/Navbar/NavbarComp";
 
 const Teams = () => {
     return (
         <div className="teamGeneral">
             <Navbar />
-            <Team />
+      
             <Footer />
         </div>
     );
